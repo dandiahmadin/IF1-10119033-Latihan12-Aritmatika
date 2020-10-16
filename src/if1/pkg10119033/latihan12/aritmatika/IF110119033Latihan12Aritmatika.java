ce461da292259e7af0e48b07c3eb6a87807edd13
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-10-16 10:52:40 +0700

    Initial Commit

---
+++ src/if1/pkg10119033/latihan12/aritmatika/IF110119033Latihan12Aritmatika.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan12.aritmatika;
+
+/**
+ *
+ * @author
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi operator aritmatika
+ */
+public class IF110119033Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        a++;
+        System.out.println("a++ = " + a);
+        b--;
+        System.out.println("b-- = " + b);
+        d++;
+        System.out.println("d++ = " + d);
+        ++d;
+        System.out.println("++d = " + d);
+    }
+    
+}
